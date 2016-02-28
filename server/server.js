a3febe236d744b21d19278cc4d9911c7edd6cdd4
Artur Siery <dotintegral@gmail.com>
2016-02-28 16:47:43 +0100

    Adding board configuration

--- server/server.js
+++ server/server.js
@@ -1,7 +1,20 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
+var fs = require('fs')
+var path = require('path')
 
-express.use('/', express.static('client'));
+var boards = []
+var boardsDir = path.join(__dirname, '..', 'boards')
+var boardConfigs = fs.readdirSync(boardsDir)
+
+boardConfigs.forEach((file) => {
+  boards.push( require(path.join(boardsDir, file)) )
+})
+
+
+boards.forEach((board) => {
+  app.use('/' + board.url, express.static('client'))
+})
 
 app.listen(8080, function () {
   console.log('Server is up and running')
