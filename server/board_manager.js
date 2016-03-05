7b68c59e191bbb13f9213dfaa8a5bf5fff1caa0e
Artur Siery <dotintegral@gmail.com>
2016-03-05 13:51:32 +0100

    Created state holder file

--- server/board_manager.js
+++ server/board_manager.js
@@ -1,42 +1,15 @@
 var path = require('path')
 var ejs = require('ejs')
 var fs = require('fs')
-var Immutable = require('immutable')
 
 var clientDir = path.join(__dirname, '..', 'client')
+var stateHolder = require('./state_holder')
 
-var boards = Immutable.Map({})
-
-
-function createBoardState(boardDefinition) {
-  boardDefinition.scenes.forEach((scene) => {
-    scene.widgets.forEach((widget) => {
-      widget.data = widget.config
-    })
-  })
-  boards = boards.set(boardDefinition.url, Immutable.fromJS(boardDefinition))
-}
-
-function getBoardState(url) {
-  return boards.get(url)
-}
-
-function boardStateString(url) {
-  var boardJSON = boards.get(url).toJSON()
-
-  boardJSON.scenes.forEach((scene) => {
-    scene.widgets.forEach((widget) => {
-      delete widget.config
-    })
-  })
-
-  return JSON.stringify(boardJSON)
-}
 
 module.exports = function boardManager(app) {
 
   function createBoard(boardDefinition) {
-    createBoardState(boardDefinition)
+    stateHolder.createBoardState(boardDefinition)
 
     var url = boardDefinition.url;
     var templateFile = path.join(clientDir, 'index.ejs')
@@ -50,9 +23,9 @@ module.exports = function boardManager(app) {
       app.use('/' + url, (req, res) => {
         res.send(
           render({
-            name: getBoardState(url).get('name'),
+            name: stateHolder.getBoardState(url).get('name'),
             url: url,
-            boardState: boardStateString(url)
+            boardState: stateHolder.boardStateString(url)
           })
         )
       })
